55251c44592560431f41f74989bf3c69e412f8e4
zouraiz523 <84671958+zouraiz523@users.noreply.github.com>
2022-03-10 20:59:41 +0500

    Add files via upload

---
+++ text typing animation javascript/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    strings: [
+    'Hello',' My name is Zouraiz.', 'I Love To Code.', 'I Love To Teach.'],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true,
+    });
